bb6cb8bc42503ced1932049b911d65225f6ccfca
yoonjaeo <2000yoonjae@gmail.com>
2024-10-21 15:34:09 +0900

    modify : experiment with multiple patent customer numbers

--- main.py
+++ main.py
@@ -6,18 +6,22 @@ import time
 load_dotenv()
 service_key = os.getenv('SERVICE_KEY')
 
-test_app = '120140558200'
+test_apps = ['120140558200', '120090359981', '120100466769']
 
 start_time = time.time()
 
-pa = patent_api.get_patent_info(service_key, test_app)
-de = design_api.get_design_info(service_key, test_app)
-tr = trademark_api.get_trademark_info(service_key, test_app)
+for app in test_apps:
+    pa = patent_api.get_patent_info(service_key, app)
+    de = design_api.get_design_info(service_key, app)
+    tr = trademark_api.get_trademark_info(service_key, app)
+
+    print("총 데이터 수 : ", len(pa) + len(de) + len(tr))
 
 end_time = time.time()
 
 print("총 걸린 시간 : ",  end_time - start_time)
-print("총 데이터 수 : ", len(pa) + len(de) + len(tr))
-print("특/실 데이터 수 : " , len(pa))
-print("디자인 데이터 수 : ", len(de))
-print("상표 데이터 수 : ", len(tr))
+
+# print("총 데이터 수 : ", len(pa) + len(de) + len(tr))
+# print("특/실 데이터 수 : " , len(pa))
+# print("디자인 데이터 수 : ", len(de))
+# print("상표 데이터 수 : ", len(tr))
