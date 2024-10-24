aac1a11c68ab047764a0d387b5d3a68166f221af
namugach <namugach@gmail.com>
2024-10-24 21:43:12 +0900

    feat: 사업자번호 기반 특허 고객번호 조회 기능 추가
    사업자번호 기반 특허 고객번호 조회 API 연동 (biz_api.py)
    데이터베이스 연결 모듈 (connection.py)
    데이터베이스 CRUD 기능 (db_crud.py)
    사업자번호 기반 특허 고객번호 매칭 스크립트 (scrpit/biz_num_to_app_num.py)
    Excel 데이터 로딩 스크립트 수정 (scrpit/load_xlsx_to_db.py)
    sql/company_info.sql 삭제 및 sql/create_table.sql 파일 추가

--- scrpit/load_xlsx_to_db.py
+++ scrpit/load_xlsx_to_db.py
@@ -1,52 +1,54 @@
-import pandas as pd
-import pandas as pd
-from sqlalchemy import create_engine, MetaData, Table
-from sqlalchemy.orm import sessionmaker
 import os
-from dotenv import load_dotenv
+import sys
+
+import pandas as pd
+import MySQLdb
 import numpy as np
 
-# .env 파일에서 DB 연결 정보를 로드
-load_dotenv()
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from crud.connection import db_connect
+from crud.db_crud import insert_data_to_db
 
-def db_connect():
-    # MySQL 연결 설정
-    DATABASE_URL = f"mysql+pymysql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}/{os.getenv('DB_NAME')}"
+# 변환 함수
+def format_biz_no(biz_no):
+    if biz_no is None:
+        return None
+    return f"{biz_no[:3]}-{biz_no[3:5]}-{biz_no[5:]}"  # 220-88-87953
 
-    # SQLAlchemy 엔진 생성
-    engine = create_engine(DATABASE_URL)
-    Session = sessionmaker(bind=engine)
-    session = Session()
-
-    return session, engine
+def format_crop_no(crop_no):
+    if crop_no is None:
+        return None
+    return f"{crop_no[:6]}-{crop_no[6:]}" 
 
 
-
-def insert_data_from_excel_to_db_com(excel_file_path , table_name):
-
-    session, engine = db_connect()
+def insert_data_from_excel_to_db_com(excel_file_path, table_name):
+    connection = db_connect()
+    cursor = connection.cursor()
 
     # 엑셀 파일 읽기
     df = pd.read_excel(excel_file_path)
-    df = df.where(pd.notnull(df), None) # Nan -> None
+    df = df.where(pd.notnull(df), None)  # Nan -> None
+
+    # biz_no와 crop_no 포맷 변경
+    df['biz_no'] = df['biz_no'].astype(str).apply(format_biz_no)
+    df['corp_no'] = df['corp_no'].apply(format_crop_no)
 
     # 데이터프레임을 딕셔너리 리스트로 변환
     data_to_insert = df.drop(columns=['company_seq']).to_dict(orient='records')
 
-    # 데이터베이스에 한꺼번에 삽입
-    with engine.connect() as connection:
-        metadata = MetaData()
-        company_info = Table(table_name, metadata,  autoload_with=engine)
-
-        # bulk insert로 한꺼번에 데이터를 삽입
-        connection.execute(company_info.insert(), data_to_insert)
-        connection.commit() 
-
+    insert_data_to_db(cursor, table_name,data_to_insert, True )
+    connection.commit()
+    
     print(f"{len(df)} records inserted successfully.")
+
+    cursor.close()
+    connection.close()
+
 
 def insert_data_from_excel_to_db_uni(excel_file_path, table_name):
 
-    session, engine = db_connect()
+    connection = db_connect()
+    cursor = connection.cursor()
 
     # 엑셀 파일 읽기
     df = pd.read_excel(excel_file_path)
@@ -56,25 +58,28 @@ def insert_data_from_excel_to_db_uni(excel_file_path, table_name):
     df['write_time'] = pd.to_datetime(df['write_time'], format='%Y-%m-%d %p %I:%M:%S', errors='coerce') # 날짜 방식 변경
     df['write_time'] = df['write_time'].dt.strftime('%Y-%m-%d %H:%M:%S')
 
+    # 형식 변경
+    df['applicant_no'] = df['applicant_no'].astype(str)
+    df['biz_no'] = df['biz_no'].apply(format_biz_no)
+    df['corp_no'] = df['corp_no'].apply(format_crop_no)
+
     df = df.replace({np.nan: None}) # Nan -> None
 
     data_to_insert = df.to_dict(orient='records')
 
-     # 데이터베이스에 한꺼번에 삽입
-    with engine.connect() as connection:
-        metadata = MetaData()
-        university_info = Table(table_name, metadata,  autoload_with=engine)
+    insert_data_to_db(cursor, table_name,data_to_insert, True )
 
-        # bulk insert로 한꺼번에 데이터를 삽입
-        connection.execute(university_info.insert(), data_to_insert)
-        connection.commit() 
-
+    connection.commit()
     print(f"{len(df)} records inserted successfully.")
+
+    cursor.close()
+    connection.close()
 
 
 if __name__ == "__main__":
-    # excel_file_path_com = '../data/TB24_100_company_info.xlsx'
-    # insert_data_from_excel_to_db_com(excel_file_path_com, 'company_info')
+    
+    excel_file_path_com = '../data/TB24_100_company_info.xlsx'
+    insert_data_from_excel_to_db_com(excel_file_path_com, 'TB24_100')
 
     excel_file_path_uni = '../data/TB24_210_university.xlsx'
-    insert_data_from_excel_to_db_uni(excel_file_path_uni , 'university_info')
+    insert_data_from_excel_to_db_uni(excel_file_path_uni , 'TB24_210')
