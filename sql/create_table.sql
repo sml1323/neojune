524922ab8d4222cb7416d22514fbe93d3a7a445a
user <user@mail.com>
2024-10-28 13:37:27 +0900

    테이블 4개(특실, 디자인, 상표, 우선권) 생성

--- sql/create_table.sql
+++ sql/create_table.sql
@@ -31,42 +31,100 @@ CREATE TABLE TB24_200(
     company_seq INT,
     FOREIGN KEY (company_seq) REFERENCES TB24_100(company_seq)
 );
-show tables;
-SELECT * FROM TB24_100;
-
 ALTER TABLE TB24_200
-ADD UNIQUE (app_no);
+ADD UNIQUE (app_no),
+ADD write_time DATETIME DEFAULT CURRENT_TIMESTAMP,
+ADD modify_time DATETIME ON UPDATE CURRENT_TIMESTAMP;
 
-CREATE TABLE TB24_300(
-    ipr_id INT AUTO_INCREMENT PRIMARY KEY,
-    ipr_seq VARCHAR(20),
-    applicant_no VARCHAR(20), 
-    biz_no VARCHAR(12),
+CREATE INDEX idx_app_no ON TB24_200(app_no);
+CREATE INDEX idx_biz_no ON TB24_200(biz_no);
+
+CREATE TABLE IF NOT EXISTS patent (
+    ipr_seq INT AUTO_INCREMENT PRIMARY KEY,
+    applicant_no VARCHAR(20) NOT NULL, -- TB24_200의 app_no 참조
+    biz_no VARCHAR(12) UNIQUE,
     ipr_code VARCHAR(5),
-    title VARCHAR(255),
+    title VARCHAR(255) NOT NULL,
+    serial_no VARCHAR(50) UNIQUE,
     applicant VARCHAR(100),
-    inventor VARCHAR(100),
-    agent VARCHAR(100),
-    ipcNumber VARCHAR(50),
-    appl_no VARCHAR(50),
+    main_ipc VARCHAR(50),
+    appl_no VARCHAR(50) UNIQUE,
     appl_date DATE,
     open_no VARCHAR(50),
     open_date DATE,
     reg_no VARCHAR(50),
     reg_date DATE,
+    notification_num VARCHAR(50),
+    notification_date DATE,
     legal_status_desc VARCHAR(20),
     abstract TEXT,
-    -- 데이터 출처 확인 필요
-    int_appl_no VARCHAR(50),
-    int_appl_date DATE,
-    int_open_no VARCHAR(50),
-    int_open_date DATE,
-    exam_flag VARCHAR(50),
-    exam_date DATE,
-    claim_cnt VARCHAR(20),
-    write_time DATETIME,
-    modify_time DATETIME,
-    survey_year DATETIME DEFAULT CURRENT_TIMESTAMP,
-    survey_month DATETIME DEFAULT CURRENT_TIMESTAMP,
+    image_path VARCHAR(255),
+    write_time DATETIME DEFAULT CURRENT_TIMESTAMP, -- 리얼타임으로 필드가 생성된 시간
+	modify_time DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, -- 수정 시간 자동 업데이트
+    survey_year VARCHAR(5), -- 최초 수집시 조사 연도
+    survey_month VARCHAR(5), -- 최초 수집시 조사 월
+    FOREIGN KEY (applicant_no) REFERENCES TB24_200(app_no),
+    FOREIGN KEY (biz_no) REFERENCES TB24_200(biz_no)
+);
+
+CREATE TABLE IF NOT EXISTS design (
+    ipr_seq INT AUTO_INCREMENT PRIMARY KEY,
+    applicant_no VARCHAR(20) NOT NULL, -- TB24_200의 app_no 참조
+    biz_no VARCHAR(12) UNIQUE,
+    ipr_code VARCHAR(5),
+    title VARCHAR(255),
+    serial_no VARCHAR(50) UNIQUE,
+    applicant VARCHAR(100),
+    inventor VARCHAR(100),
+    agent VARCHAR(100),
+    appl_no VARCHAR(50) UNIQUE,
+    appl_date DATE,
+    open_no VARCHAR(50),
+    open_date DATE,
+    reg_no VARCHAR(50),
+    reg_date DATE,
+    notification_num VARCHAR(50),
+    notification_date DATE,
+    legal_status_desc VARCHAR(20),
+    image_path VARCHAR(255),
+    write_time DATETIME DEFAULT CURRENT_TIMESTAMP, -- 리얼타임으로 필드가 생성된 시간
+	modify_time DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, -- 수정 시간 자동 업데이트
+    survey_year VARCHAR(5), -- 최초 수집시 조사 연도
+    survey_month VARCHAR(5), -- 최초 수집시 조사 월
+    FOREIGN KEY (applicant_no) REFERENCES TB24_200(app_no),
+    FOREIGN KEY (biz_no) REFERENCES TB24_200(biz_no)
+);
+
+CREATE TABLE IF NOT EXISTS trademark (
+    ipr_seq INT AUTO_INCREMENT PRIMARY KEY,
+    applicant_no VARCHAR(20) NOT NULL, -- TB24_200의 app_no 참조
+    biz_no VARCHAR(12) UNIQUE,
+    ipr_code VARCHAR(5) NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    serial_no VARCHAR(50),
+    applicant VARCHAR(100),
+    agent VARCHAR(100),
+    ipcNumber VARCHAR(50),
+    appl_no VARCHAR(50),
+    appl_date DATE,
+    notification_num VARCHAR(50),
+    notification_date DATE,
+    legal_status_desc VARCHAR(20),
+    image_path VARCHAR(255),
+    write_time DATETIME DEFAULT CURRENT_TIMESTAMP, -- 리얼타임으로 필드가 생성된 시간
+	modify_time DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, -- 수정 시간 자동 업데이트
+    survey_year VARCHAR(5), -- 최초 수집시 조사 연도
+    survey_month VARCHAR(5), -- 최초 수집시 조사 월
+    FOREIGN KEY (applicant_no) REFERENCES TB24_200(app_no),
+    FOREIGN KEY (biz_no) REFERENCES TB24_200(biz_no)
+);
+
+CREATE TABLE tb24_320 (
+    ipr_seq INT AUTO_INCREMENT PRIMARY KEY,
+    applicant_no VARCHAR(20) NOT NULL, -- TB24_200의 app_no 참조
+    priority_seq VARCHAR(20),
+    priority_nation VARCHAR(200),
+    priority_no VARCHAR(50) UNIQUE,
+    priority_date DATE,
     FOREIGN KEY (applicant_no) REFERENCES TB24_200(app_no)
 );
