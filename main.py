b286bb401c7812c7012a078ea75af1d8f409af73
namugach <namugach@mail.com>
2024-10-30 14:48:11 +0900

    리팩토링: 프로젝트 구조 재정비 및 XML 저장 기능 구현
    프로젝트 구조를 개선하고 XML 저장 기능을 추가했습니다. 사용하지 않는 파일과 스크립트를 삭제하고, 관련 모듈을 src/save_to_xml 디렉토리로 이동했습니다. 또한, XML 저장 기능을 담은 save_to.xml.py 파일을 새로 생성하고 main.py 파일을 수정하여 XML 저장 기능을 실행하도록 변경했습니다.  .gitignore 파일에 src/save_to_xml/output 디렉토리를 추가하여 생성된 XML 파일이 Git 저장소에 포함되지 않도록 설정했습니다.
    
    - 사용하지 않는 파일 및 스크립트 삭제
    - API 모듈, CRUD 모듈을 src/save_to_xml 디렉토리로 이동
    - XML 저장 기능 구현 (save_to.xml.py)
    - main.py 수정하여 XML 저장 기능 실행
    - .gitignore에 output 디렉토리 추가

--- main.py
+++ main.py
@@ -1,65 +1,3 @@
-import asyncio
-import aiohttp
-from api_modules import design_api, patent_api, trademark_api
-import os
-from dotenv import load_dotenv
-import time
-import MySQLdb
-from datetime import datetime
-import xml.etree.ElementTree as ET
-from lxml import etree
-from crud import connection, db_crud
-
-# # XML 저장 함수
-def save_data_as_xml(data_dict, file_name):
-    root = ET.Element("responseData")
-
-    for applicant_id, data in data_dict.items():
-        response_elem = ET.SubElement(root, "response")
-
-        # <applicant> 태그 추가
-        applicant_tag = ET.SubElement(response_elem, "applicant_id")
-        applicant_tag.text = str(applicant_id)
-
-        # <header> 태그 추가 (결과 코드와 메시지를 포함)
-        header = ET.SubElement(response_elem, "header")
-        ET.SubElement(header, "resultCode")
-        ET.SubElement(header, "resultMsg")
-
-        # <body> 태그 추가 및 그 안에 <items> 데이터 삽입
-        body = ET.SubElement(response_elem, "body")
-        items_elem = ET.SubElement(body, "items")
-        # data 내부에 있는 XML 콘텐츠를 <items>에 추가
-        for content in data:
-            original_data = ET.fromstring(content)
-
-            # 기존 XML에서 <items> 내부 태그들만 추가
-            items = original_data.find(".//items")
-            if items is not None:
-                for elem in items:
-                    items_elem.append(elem)
-
-    # XML 파일로 저장
-    tree = ET.ElementTree(root)
-    file_path = f"./save_to_xml/{file_name}.xml"
-    tree.write(file_path, encoding="utf-8", xml_declaration=True)
-    print(f"{file_path} 저장 완료")
-
-
-
-
-async def fetch_all_info(service_key, app_no, applicant_id, session, semaphore, pa_dict, de_dict, tr_dict):
-    async with semaphore:
-        result_patent = await patent_api.get_patent_info(service_key, app_no, session)
-        result_design = await design_api.get_design_info(service_key, app_no, session)
-        result_trademark = await trademark_api.get_trademark_info(service_key, app_no, session)
-
-        pa_dict[applicant_id] = result_patent
-        de_dict[applicant_id] = result_design
-        tr_dict[applicant_id] = result_trademark
-
-        print(f"{app_no} 총 데이터 수 : {len(pa_dict) +len(de_dict) + len(tr_dict) }")
-
 async def main():
     load_dotenv()
     service_key = os.getenv('SERVICE_KEY')
@@ -94,4 +32,4 @@ async def main():
     elapsed_time = end - start
     print(f"걸린 시간: {elapsed_time}")
 if __name__ == '__main__':
-    asyncio.run(main())
+    pass
