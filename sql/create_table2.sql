c1af3758f2daae08c3c56f6ab4141817722fbee3
yj0331yj <yj0331yj@mail.com>
2024-10-30 10:14:17 +0900

    테이블 구조 수정

--- sql/create_table2.sql
+++ sql/create_table2.sql
@@ -34,7 +34,7 @@ CREATE TABLE TB24_200 (
 -- 특허/실용신안 테이블
 CREATE TABLE IF NOT EXISTS TB24_patent (
     ipr_seq INT AUTO_INCREMENT PRIMARY KEY,
-    applicant_no VARCHAR(20) NOT NULL, -- TB24_200의 applicant_no 참조
+    applicant_id INT NOT NULL, -- TB24_200의 applicant_id 참조
     ipr_code VARCHAR(2),
     title VARCHAR(255),
     serial_no VARCHAR(50) UNIQUE,
@@ -52,16 +52,16 @@ CREATE TABLE IF NOT EXISTS TB24_patent (
     abstract TEXT,
     image_path TEXT,
     write_time DATETIME DEFAULT CURRENT_TIMESTAMP, -- 리얼타임으로 필드가 생성된 시간
-	modify_time DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, -- 수정 시간 자동 업데이트
+    modify_time DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, -- 수정 시간 자동 업데이트
     survey_year VARCHAR(5), -- 최초 수집시 조사 연도
     survey_month VARCHAR(5), -- 최초 수집시 조사 월
-    FOREIGN KEY (applicant_no) REFERENCES TB24_200(applicant_no)
+    FOREIGN KEY (applicant_id) REFERENCES TB24_200(applicant_id)
 );
 
 -- 디자인 테이블
 CREATE TABLE IF NOT EXISTS TB24_design (
     ipr_seq INT AUTO_INCREMENT PRIMARY KEY,
-    applicant_no VARCHAR(20) NOT NULL, -- TB24_200의 applicant_no 참조
+    applicant_id INT NOT NULL, -- TB24_200의 applicant_id 참조
     ipr_code VARCHAR(2),
     title VARCHAR(255),
     serial_no VARCHAR(50) UNIQUE,
@@ -79,16 +79,16 @@ CREATE TABLE IF NOT EXISTS TB24_design (
     legal_status_desc VARCHAR(20),
     image_path TEXT,
     write_time DATETIME DEFAULT CURRENT_TIMESTAMP, -- 리얼타임으로 필드가 생성된 시간
-	modify_time DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, -- 수정 시간 자동 업데이트
+    modify_time DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, -- 수정 시간 자동 업데이트
     survey_year VARCHAR(5), -- 최초 수집시 조사 연도
     survey_month VARCHAR(5), -- 최초 수집시 조사 월
-    FOREIGN KEY (applicant_no) REFERENCES TB24_200(applicant_no)
+    FOREIGN KEY (applicant_id) REFERENCES TB24_200(applicant_id)
 );
 
 -- 상표 테이블
 CREATE TABLE IF NOT EXISTS TB24_trademark (
     ipr_seq INT AUTO_INCREMENT PRIMARY KEY,
-    applicant_no VARCHAR(20) NOT NULL, -- TB24_200의 applicant_no 참조
+    applicant_id INT NOT NULL, -- TB24_200의 applicant_id 참조
     ipr_code VARCHAR(2),
     title VARCHAR(255) NOT NULL,
     serial_no VARCHAR(50),
@@ -101,31 +101,30 @@ CREATE TABLE IF NOT EXISTS TB24_trademark (
     legal_status_desc VARCHAR(20),
     image_path VARCHAR(255),
     write_time DATETIME DEFAULT CURRENT_TIMESTAMP, -- 리얼타임으로 필드가 생성된 시간
-	modify_time DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, -- 수정 시간 자동 업데이트
+    modify_time DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, -- 수정 시간 자동 업데이트
     survey_year VARCHAR(5), -- 최초 수집시 조사 연도
     survey_month VARCHAR(5), -- 최초 수집시 조사 월
-    FOREIGN KEY (applicant_no) REFERENCES TB24_200(applicant_no)
+    FOREIGN KEY (applicant_id) REFERENCES TB24_200(applicant_id)
 );
 
 -- 우선권 테이블
 CREATE TABLE TB24_320 (
     priority_seq INT AUTO_INCREMENT PRIMARY KEY,
-    applicant_no VARCHAR(20) NOT NULL, -- TB24_200의 applicant_no 참조
+    applicant_id INT NOT NULL,         -- TB24_200의 applicant_id 참조
     ipr_seq INT NOT NULL,              -- TB24_design 또는 TB24_trademark의 ipr_seq 참조
     ipr_type ENUM('design', 'trademark') NOT NULL, -- 참조 타입 구분
     priority_no VARCHAR(50) UNIQUE,    -- 우선권주장번호
     priority_date DATE,                -- 우선권주장일자
-    FOREIGN KEY (applicant_no) REFERENCES TB24_200(applicant_no),
+    FOREIGN KEY (applicant_id) REFERENCES TB24_200(applicant_id),
     CHECK (ipr_type IN ('design', 'trademark'))
 );
 
 -- IPC_CPC 테이블
 CREATE TABLE TB24_310 (
     ipc_seq INT AUTO_INCREMENT PRIMARY KEY,
-    applicant_no VARCHAR(20) NOT NULL, -- TB24_200의 applicant_no 참조
     ipr_seq INT NOT NULL, -- TB24_patent의 ipr_seq 참조
     ipc_cpc VARCHAR(10),  -- IPC_CPC 구분
     ipc_cpc_code VARCHAR(20),  -- IPC_CPC 코드(ipcNumber)
-    FOREIGN KEY (applicant_no) REFERENCES TB24_200(applicant_no),
+    FOREIGN KEY (ipr_seq) REFERENCES TB24_patent(ipr_seq),
     CHECK (ipc_cpc IN ('ipc', 'cpc'))  -- ipc_cpc 값 제한
 );
