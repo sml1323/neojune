1e1a7a98c80c7971215949e990ebbdfd34fa46e4
yj0331yj <yj0331yj@mail.com>
2024-10-31 16:13:54 +0900

    테이블 구조에 맞게 수정

--- scrpit/biz_num_to_app_num.py
+++ scrpit/biz_num_to_app_num.py
@@ -10,9 +10,7 @@ from crud.connection import db_connect
 from crud.db_crud import * 
 from api_modules.biz_api import *
 
-
 start = time.time()
-
 
 connection = db_connect()
 cursor = connection.cursor()
@@ -26,14 +24,13 @@ while True:
     rows = fetch_data_by_page(cursor, 'TB24_100', page=page, page_size=page_size)
     if rows:
         result = asyncio.run(process_applicants(service_key,rows))
+        # print(result)
+        insert_data_to_db(cursor, 'TB24_200',result, True )
+        connection.commit()
         total_data.extend(result)
         page += 1
     else:
         break
-
-
-insert_data_to_db(cursor, 'TB24_200',total_data, True )
-connection.commit()
 
 cursor.close()
 connection.close()
--- scrpit/load_xlsx_to_db.py
+++ scrpit/load_xlsx_to_db.py
@@ -20,7 +20,6 @@ def format_corp_no(corp_no):
         return None
     return f"{corp_no[:6]}-{corp_no[6:]}" 
 
-
 def insert_data_from_excel_to_db_com(excel_file_path, table_name):
     connection = db_connect()
     cursor = connection.cursor()
@@ -33,10 +32,14 @@ def insert_data_from_excel_to_db_com(excel_file_path, table_name):
     df['biz_no'] = df['biz_no'].astype(str).apply(format_biz_no)
     df['corp_no'] = df['corp_no'].apply(format_corp_no)
 
-    # 데이터프레임을 딕셔너리 리스트로 변환
-    data_to_insert = df.drop(columns=['company_seq']).to_dict(orient='records')
+    # (추가)필요한 컬럼만 선택
+    required_columns = ['biz_no', 'corp_no', 'biz_type', 'company_name']
+    df = df[required_columns]
 
-    insert_data_to_db(cursor, table_name, data_to_insert, True )
+    # (수정)데이터프레임을 딕셔너리 리스트로 변환
+    data_to_insert = df.to_dict(orient='records')
+
+    insert_data_to_db(cursor, table_name, data_to_insert, True)
     connection.commit()
     
     print(f"{len(df)} records inserted successfully.")
@@ -58,23 +61,25 @@ def insert_data_from_excel_to_db_uni(excel_file_path, table_name):
     df['write_time'] = pd.to_datetime(df['write_time'], format='%Y-%m-%d %p %I:%M:%S', errors='coerce') # 날짜 방식 변경
     df['write_time'] = df['write_time'].dt.strftime('%Y-%m-%d %H:%M:%S')
 
-    # 형식 변경
-    df['applicant_no'] = df['applicant_no'].astype(str)
+    # (수정)형식 변경
     df['biz_no'] = df['biz_no'].apply(format_biz_no)
     df['corp_no'] = df['corp_no'].apply(format_corp_no)
+
+    # (추가)필요한 컬럼만 선택
+    required_columns = ['biz_no', 'corp_no', 'uni_name']
+    df = df[required_columns]
 
     df = df.replace({np.nan: None}) # Nan -> None
 
     data_to_insert = df.to_dict(orient='records')
 
     insert_data_to_db(cursor, table_name, data_to_insert, True )
-
     connection.commit()
+    
     print(f"{len(df)} records inserted successfully.")
 
     cursor.close()
     connection.close()
-
 
 if __name__ == "__main__":
     
@@ -82,4 +87,4 @@ if __name__ == "__main__":
     insert_data_from_excel_to_db_com(excel_file_path_com, 'TB24_100')
 
     excel_file_path_uni = '../data/TB24_210_university.xlsx'
-    insert_data_from_excel_to_db_uni(excel_file_path_uni , 'TB24_210')
+    insert_data_from_excel_to_db_uni(excel_file_path_uni , 'TB24_110')
