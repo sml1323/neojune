c7ea626ee1f3a103915437a67691eac74228204f
namugach <namugach@mail.com>
2024-11-01 02:25:13 +0900

    refactor: save_data_as_xml class로 감싸기

--- src/save_to_xml/save_to_xml.py
+++ src/save_to_xml/save_to_xml.py
@@ -8,44 +8,48 @@ from ..db.mysql import Mysql
 
 mysql = Mysql()
 
-def get_timestamp():
-    return datetime.now().strftime("%Y%m%d_%H%M%S")
 
+class XMLEditor:
+    def __init__(self):
+        pass
 
-def save_data_as_xml(applicant_id, data, file_name):
-    root = ET.Element("responseData")
+    def get_timestamp(self):
+        return datetime.now().strftime("%Y%m%d_%H%M%S")
 
+    def save_data_as_xml(self, applicant_id, data, file_name):
+        # XML 루트 요소 생성
+        root = ET.Element("responseData")
+        response_elem = ET.SubElement(root, "response")
 
-    response_elem = ET.SubElement(root, "response")
+        # <applicant> 태그 추가
+        applicant_tag = ET.SubElement(response_elem, "applicant_id")
+        applicant_tag.text = str(applicant_id)
 
-    # <applicant> 태그 추가
-    applicant_tag = ET.SubElement(response_elem, "applicant_id")
-    applicant_tag.text = str(applicant_id)
+        # <header> 태그 추가 (결과 코드와 메시지를 포함)
+        header = ET.SubElement(response_elem, "header")
+        ET.SubElement(header, "resultCode")
+        ET.SubElement(header, "resultMsg")
 
-    # <header> 태그 추가 (결과 코드와 메시지를 포함)
-    header = ET.SubElement(response_elem, "header")
-    ET.SubElement(header, "resultCode")
-    ET.SubElement(header, "resultMsg")
+        # <body> 태그 추가 및 그 안에 <items> 데이터 삽입
+        body = ET.SubElement(response_elem, "body")
+        items_elem = ET.SubElement(body, "items")
 
-    # <body> 태그 추가 및 그 안에 <items> 데이터 삽입
-    body = ET.SubElement(response_elem, "body")
-    items_elem = ET.SubElement(body, "items")
-    # data 내부에 있는 XML 콘텐츠를 <items>에 추가
-    for content in data:
-        original_data = ET.fromstring(content)
+        # data 내부에 있는 XML 콘텐츠를 <items>에 추가
+        for content in data:
+            original_data = ET.fromstring(content)
 
-        # 기존 XML에서 <items> 내부 태그들만 추가
-        items = original_data.find(".//items")
-        if items is not None:
-            for elem in items:
-                items_elem.append(elem)
+            # 기존 XML에서 <items> 내부 태그들만 추가
+            items = original_data.find(".//items")
+            if items is not None:
+                for elem in items:
+                    items_elem.append(elem)
 
-    # XML 파일로 저장
-    tree = ET.ElementTree(root)
-    file_path = f"output/{get_timestamp()}/{file_name}.xml"
-    os.makedirs(os.path.dirname(file_path), exist_ok=True)
-    tree.write(file_path, encoding="utf-8", xml_declaration=True)
-    print(f"{file_path} 저장 완료")
+        # XML 파일 저장
+        file_path = f"output/{self.get_timestamp()}/{file_name}.xml"
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+        tree = ET.ElementTree(root)
+        tree.write(file_path, encoding="utf-8", xml_declaration=True)
+        print(f"{file_path} 저장 완료")
 
 
 
@@ -85,9 +89,10 @@ async def main():
     await get_run_time(get_info , f"전체 호출 완료: 3개 신청자 처리")
 
     async def save_xml():
-        save_data_as_xml(applicant_id, patent, f"patent_data")
-        save_data_as_xml(applicant_id, design, f"design_data")
-        save_data_as_xml(applicant_id, trademark, f"trademark_data")
+        xml_editor = XMLEditor()
+        xml_editor.save_data_as_xml(applicant_id, patent, f"patent_data")
+        xml_editor.save_data_as_xml(applicant_id, design, f"design_data")
+        xml_editor.save_data_as_xml(applicant_id, trademark, f"trademark_data")
     await get_run_time(save_xml , "patent_data 저장 완료")
 
 if __name__ == '__main__':
