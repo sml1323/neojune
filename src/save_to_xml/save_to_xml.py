b0920c6ebdb2e5f04d5686a59e09e9400cefe721
namugach <namugach@mail.com>
2024-11-01 02:34:11 +0900

    리팩토링: 코드 구조 개선 및 XML 편집 기능 추가
    - XMLEditor 클래스를 추가하여 XML 파일 생성 및 저장 기능을 별도로 관리합니다.
    - util 모듈을 추가하여 유틸리티 함수를 모아 관리합니다.

--- src/save_to_xml/save_to_xml.py
+++ src/save_to_xml/save_to_xml.py
@@ -1,57 +1,11 @@
-import time, asyncio, aiohttp, os
+import time, asyncio, aiohttp
 import xml.etree.ElementTree as ET
-from dotenv import load_dotenv
-from datetime import datetime
 from .api_modules import design_api, patent_api, trademark_api
 from ..db.mysql import Mysql
+from .XMLEditor import XMLEditor
 
 
 mysql = Mysql()
-
-
-class XMLEditor:
-    def __init__(self):
-        pass
-
-    def get_timestamp(self):
-        return datetime.now().strftime("%Y%m%d_%H%M%S")
-
-    def save_data_as_xml(self, applicant_id, data, file_name):
-        # XML 루트 요소 생성
-        root = ET.Element("responseData")
-        response_elem = ET.SubElement(root, "response")
-
-        # <applicant> 태그 추가
-        applicant_tag = ET.SubElement(response_elem, "applicant_id")
-        applicant_tag.text = str(applicant_id)
-
-        # <header> 태그 추가 (결과 코드와 메시지를 포함)
-        header = ET.SubElement(response_elem, "header")
-        ET.SubElement(header, "resultCode")
-        ET.SubElement(header, "resultMsg")
-
-        # <body> 태그 추가 및 그 안에 <items> 데이터 삽입
-        body = ET.SubElement(response_elem, "body")
-        items_elem = ET.SubElement(body, "items")
-
-        # data 내부에 있는 XML 콘텐츠를 <items>에 추가
-        for content in data:
-            original_data = ET.fromstring(content)
-
-            # 기존 XML에서 <items> 내부 태그들만 추가
-            items = original_data.find(".//items")
-            if items is not None:
-                for elem in items:
-                    items_elem.append(elem)
-
-        # XML 파일 저장
-        file_path = f"output/{self.get_timestamp()}/{file_name}.xml"
-        os.makedirs(os.path.dirname(file_path), exist_ok=True)
-        tree = ET.ElementTree(root)
-        tree.write(file_path, encoding="utf-8", xml_declaration=True)
-        print(f"{file_path} 저장 완료")
-
-
 
 
 def get_app_no():
