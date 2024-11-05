3da6014d0515a3441c39f837ecf462bcdc306264
yoonjaeo <2000yoonjae@gmail.com>
2024-11-05 17:55:10 +0900

    before merge dev

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ from src.test.save_to_xml import save_to_xml
 from src.test.xml_to_dict import xml_to_dict
 # from src.test.dict_to_db import dict_to_db
 
-asyncio.run(save_to_xml.main('TB24_200'))
+# asyncio.run(save_to_xml.main('TB24_200'))
 xml_to_dict.main()
 # dict_to_db.main()
