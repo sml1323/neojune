e5817d322e76ae4c129ff29fcad1715dc430ed38
namugach <namugach@mail.com>
2024-11-06 15:32:41 +0900

    테스트: KiprisFetcher 클래스 테스트 코드 개선
    KiprisFetcher 클래스 테스트 코드를 개선하여 실제 API 호출을 시뮬레이션하고 결과를 검증합니다.

--- src/test/test.py
+++ src/test/test.py
@@ -1,6 +1,8 @@
 from .kipris.parsing import param
 from .kipris.parsing import fetcher_data
+from .kipris.parsing import fetcher
 
 def run():
     # param.main()
-    fetcher_data.main()
+    # fetcher_data.main()
+    fetcher.main()
