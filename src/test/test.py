39ea0957a4ea026145ab8ea9c43d033e256f9532
namugach <namugach@mail.com>
2024-11-06 17:07:47 +0900

    이동: 업로더 테스트 코드 이동 및 이름 변경
    업로더 테스트 코드를 `dict_to_db`에서 `kipris/upload` 디렉토리로 이동하고 파일 이름을 `uploader.py`로 변경하여 테스트 코드 구조를 개선합니다.
    
    이 변경으로 업로더 관련 테스트 코드가 `kipris` 모듈 내에 위치하게 되어 코드 구성이 더욱 논리적이고 관리하기 쉬워집니다. 또한, `test.py` 파일을 수정하여 `uploader.main()` 함수를 호출하도록 변경하여 업로더 테스트를 실행할 수 있도록 합니다.

--- src/test/test.py
+++ src/test/test.py
@@ -3,10 +3,12 @@ from .kipris.parsing import fetcher_data
 from .kipris.parsing import fetcher
 from .kipris.parsing import applicant_info_fetcher
 from .kipris.convert import xml_to_dict_converter
+from .kipris.upload import uploader
 
 def run():
     # param.main()
     # fetcher_data.main()
     # fetcher.main()
     # applicant_info_fetcher.main()
-    xml_to_dict_converter.main()
+    # xml_to_dict_converter.main()
+    uploader.main()
