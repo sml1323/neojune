00048119278784452680d171e192b723ceb4a76d
namugach <namugach@mail.com>
2024-11-12 01:31:45 +0900

    수정: Kipris 데이터 업로더 테이블 이름 변경
    Kipris 데이터 업로더 클래스들의 테이블 이름을 변경했습니다.  기존의 `TB24_[데이터 종류]` 형식에서 `TB24_company_[데이터 종류]` 형식으로 변경하여 회사 관련 데이터임을 명확하게 표시합니다.
    
    - `KiprisTB24DesignDataUploader`: `TB24_design` -> `TB24_company_design`
    - `KiprisTB24PatentDataUploader`: `TB24_patent` -> `TB24_company_patent`
    - `KiprisTB24TrademarkDataUploader`: `TB24_trademark` -> `TB24_company_trademark`

--- src/kipris/upload/uploader/KiprisTB24PatentDataUploader.py
+++ src/kipris/upload/uploader/KiprisTB24PatentDataUploader.py
@@ -3,4 +3,4 @@ from ...core.upload.KiprisDataBatchUploader import KiprisDataUploader
 class KiprisTB24PatentDataUploader(KiprisDataUploader):
     def __init__(self):
         super().__init__()
-        self.table_name = "TB24_patent"
+        self.table_name = "TB24_company_patent"
