65f3bcf00b683e869f36a1908042673ffdc261f8
namugach <namugach@mail.com>
2024-11-12 11:34:31 +0900

    리팩토링: XML 변환 클래스 생성자 단순화 및 테스트 케이스 활성화
    Kipris XML 변환 클래스(`KiprisDesignXmlToDictConverter`, `KiprisPatentXmlToDictConverter`, `KiprisTrademarkXmlToDictConverter`)의 생성자를 단순화하고,  테스트 코드(`xml_to_dict_converter.py`)에서 모든 테스트 케이스를 활성화했습니다.  생성자에서 `mapper` 인자를 제거하고 내부적으로 매퍼를 생성하도록 변경하여 사용성을 개선했습니다.  모든 테스트 케이스를 활성화하여 변환 로직의 정확성을 검증합니다.
    
    - XML 변환 클래스 생성자 단순화 (mapper 인자 제거)
    - 테스트 코드의 모든 테스트 케이스 활성화 (design, patent, trademark)

--- src/kipris/convert/converter/KiprisDesignXmlToDictConverter.py
+++ src/kipris/convert/converter/KiprisDesignXmlToDictConverter.py
@@ -1,7 +1,8 @@
 from ...core.convert.KiprisXmlToDictConverter import KiprisXmlToDictConverter
 from ..cartridge.DesignKiprisConvertedDataCartridge import DesignKiprisConvertedDataCartridge
+from ..mapper.KiprisDesignMapper import KiprisDesignMapper
 
 class KiprisDesignXmlToDictConverter(KiprisXmlToDictConverter):
-    def __init__(self, mapper, xml_filename):
-        super().__init__(mapper, DesignKiprisConvertedDataCartridge, xml_filename)
+    def __init__(self, xml_filename:str=""):
+        super().__init__(KiprisDesignMapper(), DesignKiprisConvertedDataCartridge, xml_filename)
         self.item_name = "DesignInfo"
