4a560b61f79a5991d879c7900b38cde00e5725ee
namugach <namugach@mail.com>
2024-11-12 14:07:03 +0900

    정리: dict_to_sql 테스트 코드 및 SQL 쿼리 생성 클래스 개선
    `dict_to_sql` 테스트 코드를 `src/test/kipris/convert` 디렉토리로 이동하고,  `KiprisXmlDumpDataQueryBuilder` 클래스를 사용하여 SQL 쿼리를 생성하도록 개선했습니다.  이 클래스는 XML 파일을 파싱하고, 데이터를 딕셔너리로 변환한 후, SQL insert 문을 생성하는 기능을 제공합니다.  테스트 코드에서는 샘플 XML 데이터를 사용하여 SQL 쿼리 생성 로직을 검증하고, 생성된 SQL 쿼리를 `.sql` 파일에 출력합니다.  기존의 `dict_to_sql.py` 파일은 삭제했습니다.
    
    - `dict_to_sql` 테스트 코드 이동 (`src/test/dict_to_sql` -> `src/test/kipris/convert`)

--- src/test/kipris/convert/dict_to_sql.sql
+++ src/test/kipris/convert/dict_to_sql.sql
@@ -49,3 +49,18 @@ INSERT INTO TB24_company_patent
 VALUES 
 (5576, '20', '디지탈 날염기', '1', '주식회사 메카트로', 'B41J 3/407', '2020020028364', '2002-09-19', '3011244660000', '2021-08-18', '3011244660000', '2021-08-18', NULL, NULL, '거절',  '본 고안은 디지털 날염기에 관한 것으로, ... 디지털 날염기를 제공한다. 디지털 날염기, 이송구동수단, 링크결합체, 인쇄감지센서, 자동승강조절수단','http://plus.kipris.or.kr/kiprisplusws/fileToss.jsp?arg=ed43a0609e94d6e22d01c5c32ba711cfdfd0c8a3560dfe3f5c0c0fb908b6de237d7cc48779f92f863537681dac20830b'),
 (5577, '20', '디지탈1 날염기', '2', '주식2회사 메카트로', 'B41J 3/407', '20200230028364', '2002-09-19', '30112444660000', '2021-08-18', '3011244660000', '2021-08-18', NULL, NULL, '거절',  '본 고안은 디지털 날염기에 관한 것으로, ... 디지털 날염기를 제공한다. 디지털 날염기, 이송구동수단, 링크결합체, 인쇄감지센서, 자동승강조절수단','http://plus.kipris.or.kr/kiprisplusws/fileToss.jsp?arg=ed43a0609e94d6e22d01c5c32ba711cfdfd0c8a3560dfe3f5c0c0fb908b6de237d7cc48779f92f863537681dac20830b');
+
+
+
+INSERT INTO TB24_company_design (agent, appl_date, appl_no, applicant, applicant_id, image_path, inventor, ipr_code, legal_status_desc, open_date, open_no, pub_date, pub_num, reg_date, reg_no, serial_no, title)
+VALUES
+('최경수', '201340408', '30201400117534', '주식회사 파인메딕스|전성우', 2, 'http://plus.kipris.or.kr/kiprisplusws/fileToss.jsp?arg=ed43a0609e94d6e29e0b68f68d514bb0fedbb1325566a2d358de5f2bd287de10331b3d1887947c482a970947ddae1bf2bf9cda621bd8ba0d7223906319bd9903', '전성우', '30', '등록', Null, Null, '20141205', Null, '20141201', '3007743290000', '1', '내시경시술용 핸들');
+
+INSERT INTO TB24_company_design (agent, appl_date, appl_no, applicant, applicant_id, image_path, inventor, ipr_code, legal_status_desc, open_date, open_no, pub_date, pub_num, reg_date, reg_no, serial_no, title)
+VALUES
+('최경수', '2013-04-08', '30201400117534', '주식회사 파인메딕스|전성우', 2, 'http://plus.kipris.or.kr/kiprisplusws/fileToss.jsp?arg=ed43a0609e94d6e29e0b68f68d514bb0fedbb1325566a2d358de5f2bd287de10331b3d1887947c482a970947ddae1bf2bf9cda621bd8ba0d7223906319bd9903', '전성우', '30', '등록', NULL, NULL, '2014-12-05', NULL, '2014-12-01', '3007743290000', '1', '내시경시술용 핸들');
+
+
+/* ('최경수', '201420408', '30201400174517', '주식회사 파인메딕스|전성우', 2, 'http://plus.kipris.or.kr/kiprisplusws/fileToss.jsp?arg=ed43a0609e94d6e29e0b68f68d514bb0fedbb1325566a2d358de5f2bd287de10331b3d1887947c482a970947ddae1bf2bf74789afb4fa1d83bb97f3174dc9436', '전성우', '30', '등록', None, None, '20141205', None, '20141201', '3007743270000', '2', '내시경용 생검기구'),
+('최경수', '201410408', '30201400173534', '주식회사 파인메딕스|전성우', 3, 'http://plus.kipris.or.kr/kiprisplusws/fileToss.jsp?arg=ed43a0609e94d6e29e0b68f68d514bb0fedbb1325566a2d358de5f2bd287de10331b3d1887947c482a970947ddae1bf2bf9cda621bd8ba0d7223906319bd9903', '전성우', '30', '등록', None, None, '20141205', None, '20141201', '3007743290000', '1', '내시경시술용 핸들'),
+('최경수', '201450408', '30201400175517', '주식회사 파인메딕스|전성우', 3, 'http://plus.kipris.or.kr/kiprisplusws/fileToss.jsp?arg=ed43a0609e94d6e29e0b68f68d514bb0fedbb1325566a2d358de5f2bd287de10331b3d1887947c482a970947ddae1bf2bf74789afb4fa1d83bb97f3174dc9436', '전성우', '30', '등록', None, None, '20141205', None, '20141201', '3007743270000', '2', '내시경용 생검기구'); */
