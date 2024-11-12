b3292c4dccf87ef4ce82a106b89f1b0b7b830e95
namugach <namugach@mail.com>
2024-11-12 17:04:00 +0900

    추가: Kipris XML 덤프 데이터 쿼리 빌더 구현
    Kipris XML 덤프 데이터를 SQL insert 문으로 변환하는 `KiprisXmlDumpDataQueryBuilder` 클래스를 구현했습니다. 이 클래스는 XML 파일을 파싱하고, 데이터 매핑을 통해 SQL insert 문을 생성하여 파일로 저장하는 기능을 제공합니다.  테스트 코드(`dict_to_sql.py`, `dict_to_sql.sql`)를 통해 기능을 검증하고, 다양한 데이터 타입과 날짜 형식 변환을 지원합니다.
    
    - `KiprisXmlDumpDataQueryBuilder` 클래스 구현 (XML -> SQL 변환)
    - 테스트 코드 추가 및 검증
    - 날짜 형식 변환 기능 추가 (YYYYMMDD -> 'YYYY-MM-DD')
    - SQL 파일 저장 기능 구현

--- src/test/kipris/convert/dict_to_sql.py
+++ src/test/kipris/convert/dict_to_sql.py
@@ -1,7 +1,4 @@
 from ....kipris.core.convert.KiprisXmlDumpDataQueryBuilder import KiprisXmlDumpDataQueryBuilder
-from ....kipris.convert.mapper.KiprisDesignXmlMapper import KiprisDesignXmlMapper
-from ....kipris.convert.mapper.KiprisPatentXmlMapper import KiprisPatentXmlMapper
-from ....kipris.convert.mapper.KiprisTrademarkXmlMapper import KiprisTrademarkXmlMapper
 
 from ....kipris.convert.converter.KiprisDesignXmlToDictConverter import KiprisDesignXmlToDictConverter
 from ....kipris.convert.converter.KiprisPatentXmlToDictConverter import KiprisPatentXmlToDictConverter
@@ -13,16 +10,36 @@ from ....util import util
 
 def main():
     base_path = f"src/test/kipris/convert/xml"
-    design_xml_filename = f'{base_path}/design.xml'  # XML 파일 경로
+    basic_save_path = f"./res/output/{util.get_timestamp()}/sql"
+    company_path = f"{basic_save_path}/company"
+    university_path = f"{basic_save_path}/university"
 
-    # design_converter = KiprisDesignXmlToDictConverter(KiprisDesignMapper(), design_xml_filename)
-    # print(design_converter.parse())
-    printer = KiprisXmlDumpDataQueryBuilder(
+    design_xml_filename = f'{base_path}/design.xml'  # XML 파일 경로
+    patent_xml_filename = f'{base_path}/patent.xml'  # XML 파일 경로
+    trademark_xml_filename = f'{base_path}/trademark.xml'  # XML 파일 경로
+
+    company_design = KiprisXmlDumpDataQueryBuilder(
         table_name="TB24_company_design", 
         xml_filename=design_xml_filename,
-        xml_to_dict_converter_class=KiprisDesignXmlToDictConverter, 
+        xml_to_dict_converter_class=KiprisDesignXmlToDictConverter
     )
-    # printer.append()
-    printer.print_sql_file()
+    company_patent = KiprisXmlDumpDataQueryBuilder(
+        table_name="TB24_company_patent", 
+        xml_filename=patent_xml_filename,
+        xml_to_dict_converter_class=KiprisPatentXmlToDictConverter
+    )
+    company_trademark = KiprisXmlDumpDataQueryBuilder(
+        table_name="TB24_company_trademark", 
+        xml_filename=trademark_xml_filename,
+        xml_to_dict_converter_class=KiprisTrademarkXmlToDictConverter
+    )
+    # design.append()
+    # design.get_sql_file()
+    
+    company_design.save_file("design", company_path)
+    company_patent.save_file("patent", company_path)
+    company_trademark.save_file("trademark", company_path)
+
+
     # print(sql)
     pass
