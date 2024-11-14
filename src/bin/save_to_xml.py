02ea405c2493282bbedf4aa658647c76981e3337
namugach <namugach@gmail.com>
2024-11-14 16:06:11 +0900

    리팩토링: `save_to_xml` 변수명 개선
    `save_to_xml.py` 변수명을 상수 스타일로 변경했습니다.   `COMPANY_DIR_PATH`, `UNIVERSITY_DIR_PATH`, `COMPANY_APPLICANT_NUMBERS`, `UNIVERSITY_APPLICANT_NUMBERS` 변수를 상수로 선언하여 코드 가독성과 유지보수성을 향상했습니다.
    
    - 변수명 상수 스타일로 변경 (대문자 사용)

--- src/bin/save_to_xml.py
+++ src/bin/save_to_xml.py
@@ -21,22 +21,20 @@ def monitering(table_name:TableName, entity_type:KiprisXmlFileGeneratorEntityTyp
     logger.debug(table_name.value)
 
 mysql = Mysql()
-company_dir_path = "xml/company"
-university_dir_path = "xml/university"
-company_applicant_numbers = [[120140558200, 1]]
-university_applicant_numbers = [[120010134557, 1]]
-# mysql.get_all_company_no_id()
-# mysql.get_all_university_no_id()
-
-
+COMPANY_DIR_PATH = f"xml/{KiprisXmlFileGeneratorEntityType.COMPANY.value}"
+UNIVERSITY_DIR_PATH = f"xml/{KiprisXmlFileGeneratorEntityType.UNIVERSITY.value}"
+COMPANY_APPLICANT_NUMBERS = [[120140558200, 1]]
+UNIVERSITY_APPLICANT_NUMBERS = [[120010134557, 1]]
+# COMPANY_APPLICANT_NUMBERS = mysql.get_all_company_no_id()
+# UNIVERSITY_APPLICANT_NUMBERS = mysql.get_all_university_no_id()
 
 
 async def company_patent():
     monitering(TableName.TB24_200, KiprisXmlFileGeneratorEntityType.COMPANY, ApiType.PATENT)
     await KiprisPatentXmlFileGenerator(
         ApiType.PATENT.value,
-        company_dir_path,
-        company_applicant_numbers,
+        COMPANY_DIR_PATH,
+        COMPANY_APPLICANT_NUMBERS,
         KiprisXmlFileGeneratorEntityType.COMPANY
     ).save()
 
@@ -44,8 +42,8 @@ async def company_design():
     monitering(TableName.TB24_200, KiprisXmlFileGeneratorEntityType.COMPANY, ApiType.DESIGN)
     await KiprisDesignXmlFileGenerator(
         ApiType.DESIGN.value,
-        company_dir_path,
-        company_applicant_numbers,
+        COMPANY_DIR_PATH,
+        COMPANY_APPLICANT_NUMBERS,
         KiprisXmlFileGeneratorEntityType.COMPANY
     ).save()
 
@@ -53,8 +51,8 @@ async def company_trademark():
     monitering(TableName.TB24_200, KiprisXmlFileGeneratorEntityType.COMPANY, ApiType.TRADEMARK)
     await KiprisTrademarkXmlFileGenerator(
         ApiType.TRADEMARK.value,
-        company_dir_path,
-        company_applicant_numbers,
+        COMPANY_DIR_PATH,
+        COMPANY_APPLICANT_NUMBERS,
         KiprisXmlFileGeneratorEntityType.COMPANY
     ).save()
 
@@ -63,8 +61,8 @@ async def university_patent():
     monitering(TableName.TB24_210, KiprisXmlFileGeneratorEntityType.UNIVERSITY, ApiType.PATENT)
     await KiprisPatentXmlFileGenerator(
         ApiType.PATENT.value,
-        university_dir_path,
-        university_applicant_numbers,
+        UNIVERSITY_DIR_PATH,
+        UNIVERSITY_APPLICANT_NUMBERS,
         KiprisXmlFileGeneratorEntityType.UNIVERSITY
     ).save()
 
@@ -72,8 +70,8 @@ async def university_design():
     monitering(TableName.TB24_210, KiprisXmlFileGeneratorEntityType.UNIVERSITY, ApiType.DESIGN)
     await KiprisDesignXmlFileGenerator(
         ApiType.DESIGN.value,
-        university_dir_path,
-        university_applicant_numbers,
+        UNIVERSITY_DIR_PATH,
+        UNIVERSITY_APPLICANT_NUMBERS,
         KiprisXmlFileGeneratorEntityType.UNIVERSITY
     ).save()
 
@@ -81,8 +79,8 @@ async def university_trademark():
     monitering(TableName.TB24_210, KiprisXmlFileGeneratorEntityType.UNIVERSITY, ApiType.TRADEMARK)
     await KiprisTrademarkXmlFileGenerator(
         ApiType.TRADEMARK.value,
-        university_dir_path,
-        university_applicant_numbers,
+        UNIVERSITY_DIR_PATH,
+        UNIVERSITY_APPLICANT_NUMBERS,
         KiprisXmlFileGeneratorEntityType.UNIVERSITY
     ).save()
 
