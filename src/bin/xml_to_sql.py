2f112fb7be9e271d1f855f2549b55f758adbfdae
namugach <namugach@gmail.com>
2024-11-14 16:41:23 +0900

    리팩토링: `xml_to_sql` 경로 설정 및 `ApiType` Enum 분리
    `xml_to_sql` 스크립트의 XML 파일 및 SQL 저장 경로를 명확하게 설정하고, `ApiType` Enum을 별도 파일로 분리했습니다.  XML 파일 경로는 `res/output/[날짜]/xml/[회사/대학]` 형식을 사용하고, SQL 저장 경로는 `res/output/[타임스탬프]/sql/[회사/대학]` 형식을 사용합니다.  `ApiType` Enum을 `src/enum/ApiType.py`로 분리하여 코드의 재사용성과 관리 효율성을 높였습니다.
    
    - XML/SQL 파일 경로 설정 변경
    - `ApiType` Enum 분리 (`src/enum/ApiType.py`)

--- src/bin/xml_to_sql.py
+++ src/bin/xml_to_sql.py
@@ -1,4 +1,6 @@
 from enum import Enum
+from ..enum.KiprisEntityType import KiprisEntityType
+from ..enum.ApiType import ApiType
 from ..kipris.core.convert.KiprisXmlDumpDataQueryBuilder import KiprisXmlDumpDataQueryBuilder
 from ..kipris.convert.converter.KiprisDesignXmlToDictConverter import KiprisDesignXmlToDictConverter
 from ..kipris.convert.converter.KiprisPatentXmlToDictConverter import KiprisPatentXmlToDictConverter
@@ -14,67 +16,66 @@ class TableName(Enum):
     UNIVERSITY_DESIGN = "TB24_university_design"
     UNIVERSITY_TRADEMARK = "TB24_university_trademark"
 
-class ApiType(Enum):
-    PATENT = "patent"
-    DESIGN = "design"
-    TRADEMARK = "trademark"
 
+XML_DIR = "20241114"
+XML_BASE_PATH = f"res/output/{XML_DIR}/xml"
+COMPANY_XML_PATH = f"{XML_BASE_PATH}/{KiprisEntityType.COMPANY.value}"
+UNIVERSITY_XML_PATH = f"{XML_BASE_PATH}/university"
 
-BASE_PATH = f"src/test/kipris/convert/xml"
-BASIC_SAVE_PATH = f"./res/output/{util.get_timestamp()}/sql"
-COMPANY_PATH = f"{BASIC_SAVE_PATH}/company"
-UNIVERSITY_PATH = f"{BASIC_SAVE_PATH}/university"
+BASIC_SQL_PATH = f"./res/output/{util.get_timestamp()}/sql"
+COMPANY_SQL_PATH = f"{BASIC_SQL_PATH}/{KiprisEntityType.UNIVERSITY.value}"
+UNIVERSITY_SQL_PATH = f"{BASIC_SQL_PATH}/university"
 
 
 
 def run_company_design():
     query_builder = KiprisXmlDumpDataQueryBuilder(
         table_name=TableName.COMPANY_PATENT.value, 
-        xml_filename=f'{BASE_PATH}/{ApiType.DESIGN.value}.xml',  # XML 파일 경로
+        xml_filename=f'{COMPANY_XML_PATH}/{ApiType.DESIGN.value}.xml',  # XML 파일 경로
         xml_to_dict_converter_class=KiprisDesignXmlToDictConverter
     )
-    query_builder.save_file(ApiType.DESIGN.value, COMPANY_PATH)
+    query_builder.save_file(ApiType.DESIGN.value, COMPANY_SQL_PATH)
 
 def run_company_patent():
     query_builder = KiprisXmlDumpDataQueryBuilder(
         table_name=TableName.COMPANY_DESIGN.value, 
-        xml_filename=f'{BASE_PATH}/{ApiType.PATENT.value}.xml',  # XML 파일 경로
+        xml_filename=f'{COMPANY_XML_PATH}/{ApiType.PATENT.value}.xml',  # XML 파일 경로
         xml_to_dict_converter_class=KiprisPatentXmlToDictConverter
     )
-    query_builder.save_file(ApiType.PATENT.value, COMPANY_PATH)
+    query_builder.save_file(ApiType.PATENT.value, COMPANY_SQL_PATH)
 
 def run_company_trademark():
     query_builder = KiprisXmlDumpDataQueryBuilder(
         table_name=TableName.COMPANY_TRADEMARK.value, 
-        xml_filename=f'{BASE_PATH}/{ApiType.TRADEMARK.value}.xml',  # XML 파일 경로
+        xml_filename=f'{COMPANY_XML_PATH}/{ApiType.TRADEMARK.value}.xml',  # XML 파일 경로
         xml_to_dict_converter_class=KiprisTrademarkXmlToDictConverter
     )
-    query_builder.save_file(ApiType.TRADEMARK.value, COMPANY_PATH)
+    query_builder.save_file(ApiType.TRADEMARK.value, COMPANY_SQL_PATH)
 
 
 def run_university_design():
     query_builder = KiprisXmlDumpDataQueryBuilder(
         table_name=TableName.UNIVERSITY_PATENT.value, 
-        xml_filename=f'{BASE_PATH}/{ApiType.DESIGN.value}.xml',  # XML 파일 경로
+        xml_filename=f'{UNIVERSITY_XML_PATH}/{ApiType.DESIGN.value}.xml',  # XML 파일 경로
         xml_to_dict_converter_class=KiprisDesignXmlToDictConverter
     )
-    query_builder.save_file(ApiType.DESIGN.value, UNIVERSITY_PATH)
+    query_builder.save_file(ApiType.DESIGN.value, UNIVERSITY_SQL_PATH)
 
 def run_university_patent():
     query_builder = KiprisXmlDumpDataQueryBuilder(
         table_name=TableName.UNIVERSITY_DESIGN.value, 
-        xml_filename=f'{BASE_PATH}/{ApiType.PATENT.value}.xml',  # XML 파일 경로
+        xml_filename=f'{UNIVERSITY_XML_PATH}/{ApiType.PATENT.value}.xml',  # XML 파일 경로
         xml_to_dict_converter_class=KiprisPatentXmlToDictConverter
     )
-    query_builder.save_file(ApiType.PATENT.value, UNIVERSITY_PATH)
+    query_builder.save_file(ApiType.PATENT.value, UNIVERSITY_SQL_PATH)
 
 def run_university_trademark():
     query_builder = KiprisXmlDumpDataQueryBuilder(
         table_name=TableName.UNIVERSITY_TRADEMARK.value, 
-        xml_filename=f'{BASE_PATH}/{ApiType.TRADEMARK.value}.xml',  # XML 파일 경로
+        xml_filename=f'{UNIVERSITY_XML_PATH}/{ApiType.TRADEMARK.value}.xml',  # XML 파일 경로
         xml_to_dict_converter_class=KiprisTrademarkXmlToDictConverter
     )
-    query_builder.save_file(ApiType.TRADEMARK.value, UNIVERSITY_PATH)
+    query_builder.save_file(ApiType.TRADEMARK.value, UNIVERSITY_SQL_PATH)
 
 
 
