0a725e4b8bb06df1a4c8b7a42a44ec48304a8e3f
yoonjaeo <2000yoonjae@gmail.com>
2024-11-17 22:55:45 +0900

    modify: tigger 수정 version2 (제대로 된 버전)

--- res/sql/trigger_table.sql
+++ res/sql/trigger_table.sql
@@ -5,7 +5,7 @@ CREATE TABLE daily_update_company (
     service_type ENUM('design', 'trademark', 'patent') NOT NULL,
     applicant VARCHAR(300),
     legal_status_desc VARCHAR(20),
-    update_date DATE  -- 날짜를 저장할 컬럼
+    update_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP -- 날짜를 저장할 컬럼
 );
 
 CREATE TABLE daily_update_university (
@@ -14,5 +14,9 @@ CREATE TABLE daily_update_university (
     service_type ENUM('design', 'trademark', 'patent') NOT NULL,
     applicant VARCHAR(300),
     legal_status_desc VARCHAR(20),
-    update_date DATE  -- 날짜를 저장할 컬럼
+    update_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP -- 날짜를 저장할 컬럼
 );
+
+
+
+
