8514cbf4bec269cd5c642314c1bf18c6b051b61f
yoonjaeo <2000yoonjae@gmail.com>
2024-11-17 21:34:04 +0900

    modify : sql 변경

--- res/sql/trigger_table.sql
+++ res/sql/trigger_table.sql
@@ -1,4 +1,4 @@
--- Active: 1731490907969@@localhost@3306@kipris
+-- Active: 1731326975902@@kt2.elementsoft.biz@13306@kipris
 CREATE TABLE daily_update_company (
 		daily_id INT AUTO_INCREMENT PRIMARY KEY,
 		ipr_seq INT,
@@ -16,11 +16,3 @@ CREATE TABLE daily_update_university (
     legal_status_desc VARCHAR(20),
     update_date DATE  -- 날짜를 저장할 컬럼
 );
-
-INSERT INTO kipris.TB24_310_company (ipc_cpc, ipc_cpc_code, ipr_seq)
-SELECT
-'IPC', 'H05K 7/06', ipr_seq
-FROM
-TB24_company_patent
-WHERE
-appl_no = 2020040031672 AND applicant_id = 4719 AND serial_no = 1;
