87e458cf36f86c82df497867724d3f917cbce935
yj0331yj <yj0331yj@gmail.com>
2024-11-19 16:33:09 +0900

    feat: Docker 컨테이너 설정 및 requirements 파일 업데이트
    - Docker 실행 명령어에 8501, 8502 포트 추가
    - 불필요한 requirements.txt 파일 제거 (src/dashboard/streamlit/requirements.txt)
    - src/install/dev/requirements.txt에 다음 종속성 추가:
      - psycopg2-binary==2.9.10
      - XlsxWriter==3.2.0

--- install_dev.sh
+++ install_dev.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker build -f ./src/install/dev/Dockerfile -t ubuntu-neojune:24.04-kor-nmp .
 
-docker container run -itd -p 8888:80 -v ./:/root/work --name neojune ubuntu-neojune:24.04-kor-nmp
+docker container run -itd -p 8888:80 -p 8501:8501 -p 8502:8502 -v ./:/root/work --name neojune ubuntu-neojune:24.04-kor-nmp
