91f94b817c8c1f2974cbbf0965948ffda6436a0f
namugach <namugach@gmail.com>
2024-11-19 11:09:42 +0900

    리팩토링: 설치 스크립트 및 Dockerfile 구조 개선
    설치 스크립트(install_dev.sh, install_service.sh)와 Dockerfile의 경로 및 파일 구조를 개선하고, 개발 환경과 서비스 환경의 의존성 관리를 분리했습니다.
    
    - Dockerfile 및 requirements.txt 파일을 `src/install` 디렉터리 아래로 이동하여 구조를 개선했습니다.
    - 개발 환경(dev)과 서비스 환경(public)의 Dockerfile을 각각 `src/install/dev/Dockerfile`과 `src/install/public/Dockerfile`로 분리했습니다.
    - 개발 환경과 서비스 환경의 의존성 파일을 `src/install/dev/requirements.txt`와 `src/install/public/requirements.txt`로 분리하여 관리합니다.
    - `install_dev.sh` 스크립트에서 컨테이너 포트 매핑을 80:8080에서 8888:80으로 변경했습니다.
    - 더 이상 사용하지 않는 SQL 파일(neojune_2024-10-25_133204.sql)을 제거하고, Dockerfile 내에서 SQL 스크립트 실행 부분을 업데이트했습니다.

--- install_dev.sh
+++ install_dev.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker build -f ./src/dockerfile/Dockerfile_dev -t ubuntu-neojune:24.04-kor-nmp .
+docker build -f ./src/install/dev/Dockerfile -t ubuntu-neojune:24.04-kor-nmp .
 
-docker container run -itd -p 80:8080 -v ./:/root/work --name neojune ubuntu-neojune:24.04-kor-nmp
+docker container run -itd -p 8888:80 -v ./:/root/work --name neojune ubuntu-neojune:24.04-kor-nmp
