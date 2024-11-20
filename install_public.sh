b39055398526f1d2e046078f255518aa306fc275
sml1312 <eunped112@gmail.com>
2024-11-20 11:25:44 +0900

    DAG 스케줄링 및 이미지 버전 업데이트
    - Airflow DAG `all_task.py` 업데이트:
      - `pendulum`을 사용하여 시작 날짜에 타임존(`Asia/Seoul`) 추가.
      - Docker 이미지 버전을 `1.4`에서 `1.5`로 변경.
      - 태스크 그룹 구조를 리팩토링하여 가독성과 유지보수성 향상:
        - `base_processing`, `ipc_cpc_processing`, `priority_processing` 태스크 그룹을 분리.
        - 그룹 간 의존성을 간소화하여 구조 개선.

--- install_public.sh
+++ install_public.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker build -f ./src/install/public/Dockerfile -t neojune_kipris_service:3.0 .
+docker build -f ./src/install/public/Dockerfile -t neojune_kipris_service:1.5 .
 
 # docker container run -itd --name neojune_kipris_service neojune_kipris_service:2.0
--- src/airflow/dag/all_task.py
+++ src/airflow/dag/all_task.py
@@ -3,6 +3,7 @@ from airflow.providers.docker.operators.docker import DockerOperator
 from airflow.utils.task_group import TaskGroup
 from docker.types import Mount
 from datetime import datetime
+from pendulum import timezone  # pendulum의 timezone을 import
 
 # 호스트에 XML 파일이 저장될 경로
 host_output_path = "/home/ubuntu/app/res/output"
@@ -11,7 +12,7 @@ host_output_path = "/home/ubuntu/app/res/output"
 def create_task(task_id, module, task):
     return DockerOperator(
         task_id=task_id,
-        image='neojune_kipris_service:2.9',
+        image='neojune_kipris_service:1.4',
         api_version='auto',
         auto_remove=True,
         command=f'python main.py {module} {task}',
@@ -33,7 +34,7 @@ with DAG(
     default_args={'retries': 1},
     description='A DAG with DockerOperator tasks for XML, SQL, and DB processing',
     schedule_interval='0 7 * * *',
-    start_date=datetime(2023, 1, 1),
+    start_date=datetime(2023, 1, 1, tzinfo=timezone('Asia/Seoul')),
     catchup=False,
 ) as dag:
 
@@ -55,35 +56,32 @@ with DAG(
         for entity in entities:
             create_task(f'run_sql_{entity}', 'xml_to_sql', entity)
 
-    # DB 처리 태스크 그룹
-    with TaskGroup(group_id='db_processing') as db_group:
+    # Base 작업 그룹
+    with TaskGroup(group_id='base_processing') as base_group:
+        prev_task = None
+        for entity in entities:
+            task = create_task(f'run_base_{entity}', 'sql_to_db', f'base {entity}')
+            if prev_task:
+                prev_task >> task 
+            prev_task = task
 
-        # Base 작업
-        with TaskGroup(group_id='base_processing') as base_group:
-            prev_task = None
-            for entity in entities:
-                task = create_task(f'run_base_{entity}', 'sql_to_db', f'base {entity}')
-                if prev_task:
-                    prev_task >> task 
-                prev_task = task
+    # IPC/CPC 작업 그룹
+    with TaskGroup(group_id='ipc_cpc_processing') as ipc_cpc_group:
+        prev_task = None
+        for entity in ["company_patent", "university_patent"]:
+            task = create_task(f'run_ipc_cpc_{entity}', 'sql_to_db', f'ipc_cpc {entity}')
+            if prev_task:
+                prev_task >> task  
+            prev_task = task
 
-        # IPC/CPC 작업
-        with TaskGroup(group_id='ipc_cpc_processing') as ipc_cpc_group:
-            prev_task = None
-            for entity in ["company_patent", "university_patent"]:
-                task = create_task(f'run_ipc_cpc_{entity}', 'sql_to_db', f'ipc_cpc {entity}')
-                if prev_task:
-                    prev_task >> task  
-                prev_task = task
-
-        # Priority 작업
-        with TaskGroup(group_id='priority_processing') as priority_group:
-            prev_task = None
-            for entity in ["company_design", "company_trademark", "university_design", "university_trademark"]:
-                task = create_task(f'run_priority_{entity}', 'sql_to_db', f'priority {entity}')
-                if prev_task:
-                    prev_task >> task 
-                prev_task = task
+    # Priority 작업 그룹
+    with TaskGroup(group_id='priority_processing') as priority_group:
+        prev_task = None
+        for entity in ["company_design", "company_trademark", "university_design", "university_trademark"]:
+            task = create_task(f'run_priority_{entity}', 'sql_to_db', f'priority {entity}')
+            if prev_task:
+                prev_task >> task 
+            prev_task = task
 
     # dict_to_sql_sub 처리 태스크 그룹 (순차 실행)
     with TaskGroup(group_id='dict_to_sql_sub_processing') as dict_group:
@@ -95,5 +93,4 @@ with DAG(
             prev_task = task
 
     # 태스크 그룹 간 연결
-    xml_group >> sql_group >> db_group.base_processing >> dict_group >> db_group.ipc_cpc_processing >> db_group.priority_processing
-
+    xml_group >> sql_group >> base_group >> dict_group >> ipc_cpc_group >> priority_group
